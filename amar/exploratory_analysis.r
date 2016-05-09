c04f191131208a2f4c1f8e264f71a05539f996c2
AttilaZz <amarsebaa@gmail.com>
2016-05-09 17:53:57 +0200

    adding some plots - distance based plots - the distance affet too much the accuracy

--- amar/exploratory_analysis.r
+++ amar/exploratory_analysis.r
@@ -3,16 +3,80 @@
 
 
 ## let's load owr data from the data.csv file
-data <- read.csv("data.csv", stringsAsFactors = FALSE)
+data <- read.csv("data.csv", stringsAsFactors = TRUE)
+
+data$game_event_id  <- as.factor(data$game_event_id )
+data$game_id <- as.factor(data$game_id)
+data$period <- as.factor(data$period)
+data$shot_made_flag <- as.factor(data$shot_made_flag)
+data$team_id <- as.factor(data$team_id)
+data$shot_id <- as.factor(data$shot_id)
+data$playoffs <- as.factor(data$playoffs)
 
 ## we have to divide owr data into train data ( obs. that have y diffrent from NA ) and test data (obs. that have y not available)
 train <- data[!is.na(data$shot_made_flag),]
 test <- data[is.na(data$shot_made_flag),]
 
 ## since the y predictive variable is an Integer class, we should make it as factor.
-train$shot_made_flag <- as.factor(train$shot_made_flag)
+data$shot_made_flag <- as.factor(data$shot_made_flag)
 
-## let's visualize attributes names and a readble structure of the entire data:
 
-names(train)
-str(train)
+library(dplyr)
+library(ggplot2)
+
+
+
+# a plot to see position by feature
+combined_shot_type_plot <- function() {
+        f1 <- train[train$combined_shot_type == "Jump Shot",]
+        f2 <- train[train$combined_shot_type != "Jump Shot",]
+
+        ggplot() +
+                geom_point(data = f1,
+                           aes(x = lon, y = lat), color = "grey", alpha = 0.3, size = 2) +
+                geom_point(data = f2,
+                           aes(x = lon, y = lat, 
+                               color = shot_made_flag), alpha = 0.7, size = 3) +
+                ylim(c(33.7, 34.0883)) +
+                scale_color_brewer(palette = "Set1") +
+                theme_void() +
+                ggtitle("Shot Types")
+}
+
+filtered <- train[train$combined_shot_type != "Jump Shot" & train$shot_distance < 5,]
+sum(as.numeric(filtered$shot_made_flag))
+
+
+# a plot to see position by feature
+combined_shot_type_with_distance_plot <- function() {
+        
+        filtered <- train[train$combined_shot_type != "Jump Shot" & train$shot_distance < 5,]
+        ggplot() +
+                geom_point(data = filtered,
+                           aes(x =loc_x ,y = loc_y,
+                               color =shot_made_flag),
+                           alpha = 0.7, size = 3) +
+                scale_color_brewer(palette = "Set1") +
+                geom_point(aes(x =0, y = 0), size = 5, shape = 4) +
+                theme_void() +
+                ggtitle("Shots from up close (5) : 9376 : 1  | 3615 : 0 ")
+}
+
+
+train$x_bins <- cut(train$loc_x, breaks = 25)
+
+pplot <- function(feat) {
+        feat <- substitute(feat)
+        ggplot(data = train, aes_q(x = feat)) +
+                geom_bar(aes(fill = shot_made_flag), stat = "count", position = "fill") +
+                scale_fill_brewer(palette = "Set1", direction = -1) +
+                ggtitle(paste("accuracy by", feat))
+        
+}
+
+
+pplot(x_bins) + geom_bar() + ggtitle("Shot Distribution by x_bins") +
+        theme(axis.text.x = element_blank())
+
+
+pplot(x_bins) + theme(axis.text.x = element_blank())
